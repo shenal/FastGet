e269f0cee1d0412b421b922ba241d5a30f9d353f
shenal <shenal.silva007@gmail.com>
2014-08-25 16:11:07 +0530

    Code refactoring for performance enhancement

--- fastget.go
+++ fastget.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"runtime"
 	"strconv"
 	"time"
 )
@@ -18,36 +19,56 @@ var (
 
 type download struct {
 	url         string
-	dlStatus    string
+	dlStatus    chan string
 	fileName    string
 	fileStatus  string
 	connections int
+	size        int
 }
 
-func get(req *http.Request, filename string, status chan string) {
+func (dl *download) get(chunkSize int, i int) {
+	var start, end int
+	req, _ := http.NewRequest("GET", dl.url, nil)
+	if i == 0 {
+		start = 0
+	} else {
+		start = (i * chunkSize)
+	}
+
+	if i == (dl.connections - 1) {
+		end = dl.size
+	} else {
+		end = start + chunkSize - 1
+	}
+
+	fmt.Println("Request from", start, " to ", end)
+	req.Header.Set("Range", "bytes="+strconv.Itoa(start)+"-"+strconv.Itoa(end))
+
+	filename := dl.fileName + "." + strconv.Itoa(i)
 	out, _ := os.Create(filename)
 
-	client := &http.Client{}
+	client := http.Client{}
 	resp, _ := client.Do(req)
 
 	if written, err := io.Copy(out, resp.Body); err != nil {
 		fmt.Println("Error occured", err)
-		status <- "failed"
+		dl.dlStatus <- "failed"
 		return
 	} else {
 		fmt.Println("Success", filename, " written", written)
-		status <- filename + " done"
+		dl.dlStatus <- filename + " done"
 		return
 
 	}
 
 }
-func join(i int, filename string) string {
-	out, _ := os.Create(filename)
+func (dl *download) join() string {
+	i := dl.connections - 1
+	out, _ := os.Create(dl.fileName)
 	defer out.Close()
 	for j := 0; j <= i; j++ {
 
-		infile := filename + "." + strconv.Itoa(j)
+		infile := dl.fileName + "." + strconv.Itoa(j)
 		in, _ := os.Open(infile)
 		defer os.Remove(infile)
 
@@ -67,58 +88,54 @@ func join(i int, filename string) string {
 	return "done"
 }
 func main() {
-	nowTime := time.Now().Format("20060102-150405")
-	outFileName := *outFilePtr
-	if outFileName == "defaultfilename" {
-		outFileName = nowTime
-	}
-	flag.Parse()
-	url := *urlPtr
-	fmt.Println(url)
+	cpuCount := runtime.NumCPU()
+	runtime.GOMAXPROCS(cpuCount)
+	fmt.Println("using %i")
 	startTime := time.Now()
+	flag.Parse()
+	dl := download{
+		url:         *urlPtr,
+		fileName:    *outFilePtr,
+		connections: *nPtr,
+		dlStatus:    make(chan string, 1),
+	}
+	nowTime := time.Now().Format("20060102-150405")
+	if dl.fileName == "defaultfilename" {
+		dl.fileName = nowTime
+	}
 
-	n := *nPtr - 1
-	fmt.Println(n)
-	status := make(chan string, 1)
-	resp, _ := http.Get(*urlPtr)
-	i := 0
-	fileSize := resp.ContentLength
-	if isPartial := resp.Header.Get("Accept-Ranges"); isPartial != "" {
+	fmt.Println(dl.url)
 
-		chunkSize := int(fileSize) / (n + 1)
-		fmt.Println("Partials supported: ", isPartial, "\n Content size:", fileSize, "\n Chunk Size ", chunkSize)
-		end := chunkSize
-		start := 0
+	resp, _ := http.Get(dl.url)
 
-		for i = 0; i < n; i++ {
-
-			end = start + chunkSize
-			fmt.Println("Request from", start, " to ", end)
-			req, _ := http.NewRequest("GET", *urlPtr, nil)
-			req.Header.Set("Range", "bytes="+strconv.Itoa(start)+"-"+strconv.Itoa(end))
-			go get(req, outFileName+"."+strconv.Itoa(i), status)
-			start += chunkSize + 1
-
-		}
-		startOfEnd := end + 1
-		fmt.Println("Request from", end+1, " to ", fileSize)
-		req, _ := http.NewRequest("GET", *urlPtr, nil)
-		req.Header.Set("Range", "bytes="+strconv.Itoa(startOfEnd)+"-"+strconv.Itoa(int(fileSize)))
-		go get(req, nowTime+"."+strconv.Itoa(n), status)
-	} else {
+	dl.size = int(resp.ContentLength)
+	isPartial := resp.Header.Get("Accept-Ranges")
+	if isPartial == "" {
 		fmt.Println("Partial downloads not supported")
+		dl.connections = 1
+	} else {
+		fmt.Println("Partials supported: ", isPartial)
+	}
+	chunkSize := int(dl.size) / (dl.connections)
+	fmt.Println("Content size:", dl.size, "\n Chunk Size ", chunkSize)
+
+	for i := 0; i < dl.connections; i++ {
+
+		go dl.get(chunkSize, i)
 
 	}
+
 	defer resp.Body.Close()
 
-	for j := 0; j <= i; j++ {
+	for j := 0; j < dl.connections; j++ {
 
-		fmt.Println(<-status)
+		fmt.Println(<-dl.dlStatus)
 
 	}
-	join(i, nowTime)
 	timeTaken := time.Since(startTime).Seconds()
-	speed := (float64(fileSize / 1024)) / timeTaken
-	fmt.Printf("done time taken:  %.3f seconds , %.2f Speed:kB/s", timeTaken, speed)
+	dl.join()
+
+	speed := (float64(dl.size / 1024)) / timeTaken
+	fmt.Printf("done time taken:  %.3f seconds , %.2f Speed:kB/s\n", timeTaken, speed)
 
 }
